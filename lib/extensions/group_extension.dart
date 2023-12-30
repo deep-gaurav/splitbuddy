abdcbf382395a1185e43b15ecef1242686ffa1a9
Deep Gaurav <deepgauravraj@gmail.com>
2023-12-30 22:49:02 +0530

    fix name

--- lib/extensions/group_extension.dart
+++ lib/extensions/group_extension.dart
@@ -3,5 +3,5 @@ import 'package:splitbuddy/graphql/__generated__/queries.data.gql.dart';
 
 extension GroupExtension on GGroupFields {
   String get displayName =>
-      name ?? members.map((p0) => p0.displayName).join(", ");
+      name ?? members.map((p0) => p0.shortName).join(", ");
 }
--- lib/extensions/user_extension.dart
+++ lib/extensions/user_extension.dart
@@ -2,4 +2,9 @@ import 'package:splitbuddy/graphql/__generated__/queries.data.gql.dart';
 
 extension UserExtension on GUserFields {
   String get displayName => name ?? email ?? phone ?? id;
+  String get shortName =>
+      name?.split(" ").firstOrNull ??
+      email?.split('@').firstOrNull ??
+      phone ??
+      id;
 }
