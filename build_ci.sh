8e3dce0d495dc1594f2a425a5596f76ffba411d6
Deep Gaurav <deepgauravraj@gmail.com>
2024-01-27 21:52:55 +0530

    add enpoint variable

--- build_ci.sh
+++ build_ci.sh
@@ -1,2 +1,2 @@
 git clone -b stable https://github.com/flutter/flutter.git
-./flutter/bin/flutter build web --release --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY"
+./flutter/bin/flutter build web --release --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
