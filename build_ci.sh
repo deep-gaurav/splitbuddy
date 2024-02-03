77a9cee92566dcf47150eea39cda1313bc3e9c95
Deep Gaurav <deepgauravraj@gmail.com>
2024-02-03 09:58:49 +0530

    add robots.txt

--- build_ci.sh
+++ build_ci.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
 git clone -b stable https://github.com/flutter/flutter.git
 ./flutter/bin/flutter build web --release --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
+
+
+# Generate robots.txt based on the current branch
+if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
+    # Allow everything in robots.txt for the 'main' branch
+    echo "User-agent: *" > build/web/robots.txt
+    echo "Disallow:" >> build/web/robots.txt
+else
+    # Disallow everything in robots.txt for other branches
+    echo "User-agent: *" > build/web/robots.txt
+    echo "Disallow: /" >> build/web/robots.txt
+fi
