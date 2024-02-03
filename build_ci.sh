5be4c568938817741da2fbd984e43394e7ff964e
Deep Gaurav <deepgauravraj@gmail.com>
2024-02-03 10:09:59 +0530

    use env var

--- build_ci.sh
+++ build_ci.sh
@@ -4,13 +4,13 @@ git clone -b stable https://github.com/flutter/flutter.git
 ./flutter/bin/flutter build web --release --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
 
 
-# Generate robots.txt based on the current branch
-if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
-    # Allow everything in robots.txt for the 'main' branch
+# Generate robots.txt based on the IS_PROD environment variable
+if [ "$IS_PROD" = "true" ]; then
+    # Allow everything in robots.txt for production build
     echo "User-agent: *" > build/web/robots.txt
     echo "Disallow:" >> build/web/robots.txt
 else
-    # Disallow everything in robots.txt for other branches
+    # Disallow everything in robots.txt for other builds
     echo "User-agent: *" > build/web/robots.txt
     echo "Disallow: /" >> build/web/robots.txt
 fi
