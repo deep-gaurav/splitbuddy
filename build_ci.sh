2ac3023e450f4534010e78c5f124b3222ba17b17
Deep Gaurav <deepgauravraj@gmail.com>
2024-02-05 19:43:28 +0530

    fix robots

--- build_ci.sh
+++ build_ci.sh
@@ -4,19 +4,19 @@ git clone -b stable https://github.com/flutter/flutter.git
 ./flutter/bin/flutter build web --release --base-href="/app/" --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
 
 
-# Generate robots.txt based on the IS_PROD environment variable
-if [ "$IS_PROD" = "true" ]; then
-    # Allow everything in robots.txt for production build
-    echo "User-agent: *" > build/web/robots.txt
-    echo "Disallow:" >> build/web/robots.txt
-else
-    # Disallow everything in robots.txt for other builds
-    echo "User-agent: *" > build/web/robots.txt
-    echo "Disallow: /" >> build/web/robots.txt
-fi
-
 cd landing_page/billdivide
 npm install
 npm run build
 
 cp -r ../../build/web ./dist/app
+
+# Generate robots.txt based on the IS_PROD environment variable
+if [ "$IS_PROD" = "true" ]; then
+    # Allow everything in robots.txt for production build
+    echo "User-agent: *" > dist/robots.txt
+    echo "Disallow:" >> dist/robots.txt
+else
+    # Disallow everything in robots.txt for other builds
+    echo "User-agent: *" > dist/robots.txt
+    echo "Disallow: /" >> dist/robots.txt
+fi
