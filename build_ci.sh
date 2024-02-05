531cf37e9bc68f1b195f12385eba46d65f75bb79
Deep Gaurav <deepgauravraj@gmail.com>
2024-02-05 17:36:21 +0530

    make ci for new site

--- build_ci.sh
+++ build_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone -b stable https://github.com/flutter/flutter.git
-./flutter/bin/flutter build web --release --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
+./flutter/bin/flutter build web --release --base-href="/app/" --web-renderer html --no-web-resources-cdn --pwa-strategy none --dart-define "VAPID_KEY=$VAPID_KEY" --dart-define "ENDPOINT=$ENDPOINT"
 
 
 # Generate robots.txt based on the IS_PROD environment variable
@@ -14,3 +14,9 @@ else
     echo "User-agent: *" > build/web/robots.txt
     echo "Disallow: /" >> build/web/robots.txt
 fi
+
+cd landing_page/billdivide
+npm install
+npm run build
+
+cp -r ../../build/web ./dist/app
